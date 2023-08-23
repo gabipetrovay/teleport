ba629d490fe017d8078c82a0e8dced722a433e4b
Rafał Cieślak <rafal.cieslak@goteleport.com>
2023-08-23 17:58:10 +0200

    Properly handle error from retryWithRelogin

--- web/packages/teleterm/src/ui/ConnectMyComputer/DocumentConnectMyComputerSetup/DocumentConnectMyComputerSetup.tsx
+++ web/packages/teleterm/src/ui/ConnectMyComputer/DocumentConnectMyComputerSetup/DocumentConnectMyComputerSetup.tsx
@@ -152,31 +152,33 @@ function AgentSetup({ rootClusterUri }: { rootClusterUri: RootClusterUri }) {
 
   const [createRoleAttempt, runCreateRoleAttempt, setCreateRoleAttempt] =
     useAsync(
-      useCallback(async () => {
-        retryWithRelogin(ctx, rootClusterUri, async () => {
-          let certsReloaded = false;
+      useCallback(
+        () =>
+          retryWithRelogin(ctx, rootClusterUri, async () => {
+            let certsReloaded = false;
 
-          try {
-            const response = await ctx.connectMyComputerService.createRole(
-              rootClusterUri
-            );
-            certsReloaded = response.certsReloaded;
-          } catch (error) {
-            if (isAccessDeniedError(error)) {
-              throw new Error(
-                'Access denied. Contact your administrator for permissions to manage users and roles.'
+            try {
+              const response = await ctx.connectMyComputerService.createRole(
+                rootClusterUri
               );
+              certsReloaded = response.certsReloaded;
+            } catch (error) {
+              if (isAccessDeniedError(error)) {
+                throw new Error(
+                  'Access denied. Contact your administrator for permissions to manage users and roles.'
+                );
+              }
+              throw error;
             }
-            throw error;
-          }
 
-          // If tshd reloaded the certs to refresh the role list, the Electron app must resync details
-          // of the cluster to also update the role list in the UI.
-          if (certsReloaded) {
-            await ctx.clustersService.syncRootCluster(rootClusterUri);
-          }
-        });
-      }, [ctx, rootClusterUri])
+            // If tshd reloaded the certs to refresh the role list, the Electron app must resync details
+            // of the cluster to also update the role list in the UI.
+            if (certsReloaded) {
+              await ctx.clustersService.syncRootCluster(rootClusterUri);
+            }
+          }),
+        [ctx, rootClusterUri]
+      )
     );
   const [
     generateConfigFileAttempt,
